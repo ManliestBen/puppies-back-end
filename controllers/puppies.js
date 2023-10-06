17e2059f60124a032fa9833950798673f111b9bc
Ben Manley <benjamintmanley@gmail.com>
2023-10-06 13:39:49 -0500

    full CRUD

--- controllers/puppies.js
+++ controllers/puppies.js
@@ -11,6 +11,50 @@ async function create(req, res) {
   }
 }
 
+async function index(req, res) {
+  try {
+    const puppies = await Puppy.find({})
+    res.json(puppies)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
+
+async function show(req, res) {
+  try {
+    const puppy = await Puppy.findById(req.params.puppyId)
+    res.json(puppy)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
+
+async function deletePuppy(req, res) {
+  try {
+    const puppy = await Puppy.findByIdAndDelete(req.params.puppyId)
+    res.json(puppy)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
+
+async function update(req, res) {
+  try {
+    const puppy = await Puppy.findByIdAndUpdate(req.params.puppyId, req.body, {new: true})
+    res.json(puppy)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
+
 export {
-  create
+  create,
+  index,
+  show,
+  update,
+  deletePuppy as delete
 }
--- routes/puppies.js
+++ routes/puppies.js
@@ -5,7 +5,12 @@ const router = Router()
 
 // ========== Public Routes ===========
 
+router.get('/', puppiesCtrl.index)
+router.get('/:puppyId', puppiesCtrl.show)
 router.post('/', puppiesCtrl.create)
+router.put('/:puppyId', puppiesCtrl.update)
+router.delete('/:puppyId', puppiesCtrl.delete)
+
 
 export {
   router
